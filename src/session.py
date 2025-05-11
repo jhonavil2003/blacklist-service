65cb573b97e4ae8269a2da10ce2d09d8e29e662c
Jhon Avila <jf.avila@uniandes.edu.co>
2025-05-11 14:44:44 -0500

    Entrega continua fallida

--- src/session.py
+++ src/session.py
@@ -21,7 +21,7 @@ class SessionConfig():
         db_pass = os.environ['RDS_PASSWORD']
         db_host = os.environ['RDS_HOSTNAME']
         db_port = os.environ['RDS_PORT']
-        db_name = os.environ['RDS_DB_NAME']
+        db_name = os.environ['RDS_DB_NAMEE']
         return f'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 session_config = SessionConfig()
