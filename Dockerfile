d9785cf7014ab3e131bfb2f1e9128aa5ad8ca9ed
Jhon Avila <jf.avila@uniandes.edu.co>
2025-05-11 03:49:26 -0500

    Ajustes entrega 3

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY . .
 
 RUN pip install pipenv && pipenv install --deploy --system
 
-EXPOSE 80
+EXPOSE 5000
 
-CMD ["sh", "-c", "FLASK_APP=./src/main.py flask run -h 0.0.0.0 -p 80"]
+CMD ["sh", "-c", "FLASK_APP=./src/main.py flask run -h 0.0.0.0 -p 5000"]
--- src/session.py
+++ src/session.py
@@ -11,18 +11,9 @@ class SessionConfig():
         pass
 
     def url(self):
-        if os.environ.get("ENV") == "testing":
-            return os.environ.get("DB_URL", "sqlite:///:memory:")
+        return os.environ.get("DB_URL", "sqlite:///:memory:")
     
-        db_url = os.getenv("DB_URL")
-        if db_url:
-            return db_url
-        db_user = os.environ['RDS_USERNAME']
-        db_pass = os.environ['RDS_PASSWORD']
-        db_host = os.environ['RDS_HOSTNAME']
-        db_port = os.environ['RDS_PORT']
-        db_name = os.environ['RDS_DB_NAME']
-        return f'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+
 
 session_config = SessionConfig()
 engine = create_engine(session_config.url())
